65a2ddbbad995083ef530bc3ac50c82a1d50cebc
Sennevds <senne.vandesompele@student.artesis.be>
2013-10-26 14:52:33 +0200

    Added basic login page has to be implemented with azure

--- MM Systems/PanoramaApp3/PanoramaApp3/Pages/Settings.xaml.cs
+++ MM Systems/PanoramaApp3/PanoramaApp3/Pages/Settings.xaml.cs
@@ -5,8 +5,10 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using Coding4Fun.Toolkit.Controls;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using PanoramaApp3.Pages;
 
 namespace PanoramaApp3
 {
@@ -15,6 +17,31 @@ namespace PanoramaApp3
         public Page1()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var luc = new WindowsPhoneControl1(); // Customer user control with Login UI
+
+            MessagePrompt messagePrompt = new MessagePrompt(); // Coding4Fun extensible MessagePrompt
+            messagePrompt.IsCancelVisible = true; // Show cancel button
+
+            messagePrompt.Body = luc; // Add user control as body of MessagePrompt
+            messagePrompt.Completed += (str, res) => // Handler for MessagePrompt user action
+            {
+                if (res.PopUpResult == PopUpResult.Cancelled)
+                {
+                    luc.Username = luc.Password = "";
+                    MessageBox.Show("Login Cancelled");
+                }
+                else
+                {
+                    MessageBox.Show(string.Format("Username: {0}, Password: {1}", luc.Username, luc.Password));
+                }
+            };
+
+            messagePrompt.Show(); 
+
         }
     }
 }
