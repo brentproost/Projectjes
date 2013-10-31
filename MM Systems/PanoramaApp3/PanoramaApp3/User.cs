3a97629195befce4a329f9f32c71a79d714c2802
brentproost <brent_proost@hotmail.com>
2013-10-31 15:10:12 +0100

    static class add

---
+++ MM Systems/PanoramaApp3/PanoramaApp3/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PanoramaApp3
+{
+    public static class User
+    {
+        public static int ID { get; set; }
+        public static string Naam { get; set; }
+        public static string Voornaam { get; set; }
+        public static string Adres { get; set; }
+        public static int Nummer { get; set; }
+        public static string Plaats { get; set; }
+        public static int Postcode { get; set; }
+        public static string Gebruikersnaam { get; set; }
+        public static string Paswoord { get; set; }
+        public static int Rechten_ID { get; set; }
+    }
+}
